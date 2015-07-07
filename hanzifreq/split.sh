e81d73ff1f77216184542df868c5ff2dc774ab0f
Christian Zielinski <email@czielinski.de>
2015-07-07 11:21:31 +0800

    Improved split.sh script

--- hanzifreq/split.sh
+++ hanzifreq/split.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 path/to/large_file" >&2
+  echo >&2 "Usage: $0 path/to/large.file"
+  echo >&2 "This script takes a large file and splits it"
+  echo >&2 "into smaller files in the directory 'input/'"
   exit 1
 fi
+
+type split >/dev/null 2>&1 || {
+  echo >&2 "ERROR: Please install 'split'."
+  exit 1
+}
 
 split --lines=500000 "$1" "input/part_"
